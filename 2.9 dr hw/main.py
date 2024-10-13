39006fd4027c9bc0af806e60ba12c752f5a59676
Insomnia01001 <167763964+Insomnia01001@users.noreply.github.com>
2024-10-13 23:49:51 +0300

    Add files via upload

---
+++ 2.9 dr hw/main.py
@@ -0,0 +1,52 @@
+# int1 = int(input("son kiriting"))
+# while int1:
+#     print(int1)
+#     int1-=1
+# int2 = int(input("son"))
+
+
+# chunmaganim
+# n = int(input("Iltimos, biror son kiriting: "))
+
+# Faktorialni hisoblash uchun o'zgaruvchilarni belgilaymiz
+# faktorial = 1
+# i = 1
+
+# # Faktorialni hisoblash uchun while siklidan foydalanamiz
+# while i <= n:
+#     faktorial *= i  # i ni faktorialga ko'paytiramiz
+#     i += 1  # i ni 1 ga oshiramiz
+
+# # Natijani chiqaramiz
+# print(f"{n} sonining faktoriali: {faktorial}")
+
+
+
+
+
+# int3 = int(input("son"))
+# i = 1
+# while i<=int3:
+#     i
+
+
+# 2
+# son = input("Istalgan sonni kiriting: ")
+
+# Raqamlar yig'indisini hisoblash
+# yigindi = 0
+# i = 0
+
+# while i < len(son):
+#     yigindi += int(son[i])
+#     i += 1
+
+# # Natijani chiqarish
+# print(f"{son} raqamlar yig'indisi: {yigindi}")
+
+
+inp4 = input("ismingizni kititing")
+ism = 0
+while ism <= len(inp4):
+    print(inp4[ism])
+    ism+=1
